731aad6ea58321e4b909e67b1ac92abed8df0903
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-23 11:57:35 -0300

    altera variável para constante

--- src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DesafioApplication {
+    private static final int  MAX_VALUE = 350;
 
     public static List<Integer> fibonacci() {
         List<Integer> fibonacciNumbers = new ArrayList<>();
 
-        int maxValue = 350;
         int previousNumber = 0;
         int currentNumber = 1;
         int nextNumber = 0;
@@ -16,7 +16,7 @@ public class DesafioApplication {
         fibonacciNumbers.add(previousNumber);
         fibonacciNumbers.add(currentNumber);
 
-        while (currentNumber < maxValue) {
+        while (currentNumber < MAX_VALUE) {
             nextNumber = currentNumber + previousNumber;
             fibonacciNumbers.add(nextNumber);
             previousNumber = currentNumber;
